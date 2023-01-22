5858c23197336497fc27978f69f6f11d5d65f07f
Cormac McGaughey <cormac.mcgaughey@gmail.com>
2023-01-21 19:38:50 -0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# AmstradCPCRom
+# Amstrad CPC Rom
 Just a repository of scripts for working with Amstrad CPC Roms
+
+All files are Python3
+
+- dum.py
+
+This dumps a .ROM file showing any printable ascii characters and strings. It takes an optional xor value for looking
+at the encoded information in Graduate Software CPM+ roms.
+
+Usage: ./dum.py <filename> <xor value>
---
+++ dum.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2023 Cormac McGaughey
+#
+# GNU GENERAL PUBLIC LICENSE
+#    Version 3, 29 June 2007
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import sys
+
+dumpname=sys.argv[1]
+xorval=sys.argv[2]
+
+print ("xorval="+xorval)
+
+loc = 0
+with open(dumpname, "rb") as f:
+    while (byte := f.read(1)):
+        # Do stuff with byte.
+        #print(loc%64)
+        value=int.from_bytes(byte,"little")
+        #value=value^0xaa #password
+        #value=value^0x4e #name etc
+        if ((loc%64)==0):
+            print ("\n"+hex(loc)+": ", end='')
+        loc = loc + 1
+        if (value>31 and value<127):
+            print(chr(value),end='')
+        else:
+            print(".",end='')
