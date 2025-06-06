4dd5c4674d4188afe45d7411a27fd60fbd697339
Cormac McGaughey <cormac.mcgaughey@gmail.com>
2025-06-05 21:35:25 -0700

    refactor all the things

--- cpmrompatch.py
+++ cpmrompatch.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-# Copyright (c) 2023 Cormac McGaughey
-#
+# Copyright (c) 2023,2025 Cormac McGaughey
+# Licensed under the GNU GPL v3.
+
 # GNU GENERAL PUBLIC LICENSE
 #    Version 3, 29 June 2007
 #
@@ -19,317 +20,136 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-# This program is designed to patch the Graduate software CPM+ rom for Amstrad CPC systems
-# This was written has a quick hack and I really need to refactor it along the lines of what
-# I did for the accessory rom builder and pull it all into ram, patch it and write it out.
 
 import sys
+import argparse
+from pathlib import Path
 
+ROM_SIZE = 16384
+PATCH_OFFSETS = {
+    0xBE: 0x0, 0xC2: 0x0, 0xC3: 0x0, 0xC4: 0x0, 0xCE: 0x0, 0xCF: 0x0,
+    0xD0: 0x0, 0x19B: 0xD, 0x19C: 0xA, 0x19D: 0x20, 0x19E: 0x47, 0x19F: 0x72,
+    0x1A0: 0x61, 0x1A1: 0x64, 0x1A2: 0x75, 0x1A4: 0x74, 0x1A5: 0x65, 0x1A6: 0x20,
+    0x1A7: 0x53, 0x1A8: 0x6F, 0x1A9: 0x66, 0x1AA: 0x74, 0x1AB: 0x77, 0x1AC: 0x61,
+    0x1AD: 0x72, 0x1AE: 0x65, 0x1AF: 0x20, 0x1B0: 0x2D, 0x1B1: 0x20, 0x1B2: 0x43,
+    0x1B3: 0x50, 0x1B4: 0x2F, 0x1B5: 0x4D, 0x1B6: 0x2B, 0x1B7: 0x20, 0x1B8: 0x6C,
+    0x1B9: 0x6F, 0x1BA: 0x61, 0x1BB: 0x64, 0x1BC: 0x65, 0x1BD: 0x72, 0x1BE: 0xC9,
+    0x1C1: 0xC9
+}
 
-# function definition
 def xorcrypt(data, xorval, padlevel):
-    result = ""
-    for element in range(0, len(data)):
-        result = result + chr(ord(data[element]) ^ xorval)
-    # pad the rest with zeroes
-    for pad in range(len(data) + 1, padlevel):
-        result = result + chr(0 ^ xorval)  # pad out with zeroes
-    result = bytes(result, "latin-1")
-    return result
+    result = bytearray([b ^ xorval for b in data.encode("latin-1")])
+    result.extend([0 ^ xorval] * (padlevel - len(data)))
+    return bytes(result)
 
+def validate(field, value, maxlen):
+    if len(value) > maxlen:
+        raise ValueError(f"{field} is too long. Maximum is {maxlen} characters, got {len(value)}.")
 
-def validate(what, srcstring, length):
-    if len(srcstring) > length:
-        print(
-            "Error: " + what + " is too long. Maximum characters is",
-            length,
-            " Currently it's at ",
-            len(srcstring),
-            "characters.",
-        )
-        quit()
-    return
-
-
-def dumphelp():
-    print("Usage:")
-    print("To display the details of what is stored in the ROM:")
-    print("  cpmrompatch.py --src SourceROM.rom --display\n")
-    print(
-        "To update the ROM, you must specify --src and --dest and one of the following:"
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Patch/display Graduate Software CPM+ ROMs for Amstrad CPC."
     )
-    print("  cpmrompatch.py --src SourceROM.rom --dest DestROM.rom")
-    print(
-        "    --quiet - Disable the noisy, large default ROM boot message and replace it with a short version"
+    parser.add_argument("--src", help="Source ROM file")
+    parser.add_argument("--dest", help="Destination ROM file (for patching)")
+    parser.add_argument("--display", action="store_true", help="Display ROM details")
+    parser.add_argument("--quiet", action="store_true", help="Patch ROM to quiet boot")
+    parser.add_argument("--name", help="Set ROM owner name (max 24 chars)")
+    parser.add_argument("--address", help="Set ROM owner address (max 68 chars)")
+    parser.add_argument("--serial", help="Set serial number (max 12 chars)")
+    parser.add_argument("--password", help="Set password (max 16 chars)")
+    parser.add_argument("filename", nargs="?", help="ROM file (display mode if no --src)")
+
+    args = parser.parse_args()
+    # For display mode with just a filename
+    if not args.src and args.filename:
+        args.src = args.filename
+        args.display = True
+    return args
+
+def extract_fields(rom_bytes):
+    # Extract copyright, name, address, serial and password as in original
+    copyright = "".join(
+        chr(rom_bytes[i] ^ 0x00)
+        for i in range(0x1FF0, 0x200A)
+        if 31 < (rom_bytes[i] ^ 0x00) < 127
     )
-    print('    --name "<Name>" - Sets the name of the ROM owner')
-    print('    --address "<Address>" - Sets the address of the ROM owner')
-    print("    --serial <serial number> - Sets the serial number of the ROM")
-    print("    --password <password> - Sets the password for |PASSWORD\n")
-    print(
-        "You can also just pass a filename and it will display the details for that ROM"
+    name = "".join(
+        chr(rom_bytes[i] ^ 0x4E)
+        for i in range(0x3F00, 0x3F17)
+        if 31 < (rom_bytes[i] ^ 0x4E) < 127
     )
-    print("  eg cpmrompatch.py CPM1.ROM\n")
-    print("Examples:")
-    print(
-        '   cpmrompatch.py --src CPM1.rom --dest CPM1-updated.rom --name "John Smith" --address "123 Acacia Ave, Sometown"'
+    address = "".join(
+        chr(rom_bytes[i] ^ 0x4E)
+        for i in range(0x3F1A, 0x3F5E)
+        if 31 < (rom_bytes[i] ^ 0x4E) < 127
     )
-    print("   cpmrompatch.py --src CPM1.rom --dest CPM1-updated.rom --quiet")
-    print("\n")
-    quit()
-    return
+    serial = "".join(
+        chr(rom_bytes[i] ^ 0x4E)
+        for i in range(0x3F71, 0x3F7D)
+        if 31 < (rom_bytes[i] ^ 0x4E) < 127
+    )
+    pwlength = rom_bytes[0x3F87] ^ 0xAA
+    password = "".join(
+        chr(rom_bytes[i] ^ 0xAA)
+        for i in range(0x3F88, 0x3FFF)
+        if 31 < (rom_bytes[i] ^ 0xAA) < 127
+    )
+    return copyright, name, address, serial, password, pwlength
 
-
-argc = len(sys.argv)
-
-if argc == 1:
-    dumphelp()
-
-loc = 0
-name = ""
-address = ""
-serial = ""
-pwlength = 0
-password = ""
-copyright = ""
-
-# Blank the parameters, just in case
-newname = ""
-newname_crypt = ""
-
-newpw = ""
-newpw_crypt = ""
-newpwlen_crypt = 0
-
-newaddress = ""
-newaddress_crypt = ""
-
-newserial = ""
-newserial_crypt = ""
-
-src = ""
-dest = ""
-
-# Mark parameters as unset for later sanity checks
-display = -1
-setpw = -1
-setname = -1
-setaddr = -1
-sourceset = -1
-setserial = -1
-destset = -1
-quietpatch = -1
-
-# So.. this is basically a glorified patch file.
-# These changes turn off all the graduate software noise and id and colours.
-# It just makes it print a single string reading:
-# "Graduate Software - CP/M+ loader."
-# Define an array the length of the rom
-patchfile = bytearray(16384)
-
-# First make everything invalid
-for loop in range(len(patchfile)):
-    patchfile[loop] = 0xFF
-
-# Now update the array with the needed changes.
-patchfile[0xBE] = 0x0
-patchfile[0xC2] = 0x0
-patchfile[0xC3] = 0x0
-patchfile[0xC4] = 0x0
-patchfile[0xCE] = 0x0
-patchfile[0xCF] = 0x0
-patchfile[0xD0] = 0x0
-patchfile[0x19B] = 0xD
-patchfile[0x19C] = 0xA
-patchfile[0x19D] = 0x20
-patchfile[0x19E] = 0x47
-patchfile[0x19F] = 0x72
-patchfile[0x1A0] = 0x61
-patchfile[0x1A1] = 0x64
-patchfile[0x1A2] = 0x75
-patchfile[0x1A4] = 0x74
-patchfile[0x1A5] = 0x65
-patchfile[0x1A6] = 0x20
-patchfile[0x1A7] = 0x53
-patchfile[0x1A8] = 0x6F
-patchfile[0x1A9] = 0x66
-patchfile[0x1AA] = 0x74
-patchfile[0x1AB] = 0x77
-patchfile[0x1AC] = 0x61
-patchfile[0x1AD] = 0x72
-patchfile[0x1AE] = 0x65
-patchfile[0x1AF] = 0x20
-patchfile[0x1B0] = 0x2D
-patchfile[0x1B1] = 0x20
-patchfile[0x1B2] = 0x43
-patchfile[0x1B3] = 0x50
-patchfile[0x1B4] = 0x2F
-patchfile[0x1B5] = 0x4D
-patchfile[0x1B6] = 0x2B
-patchfile[0x1B7] = 0x20
-patchfile[0x1B8] = 0x6C
-patchfile[0x1B9] = 0x6F
-patchfile[0x1BA] = 0x61
-patchfile[0x1BB] = 0x64
-patchfile[0x1BC] = 0x65
-patchfile[0x1BD] = 0x72
-patchfile[0x1BE] = 0xC9
-patchfile[0x1C1] = 0xC9
-
-values = range(argc)
-
-# Process the command line parameters
-for param in values:
-    if sys.argv[param] == "--display":
-        display = 1
-    elif (sys.argv[param] == "-h") or (sys.argv[param] == "--help"):
-        dumphelp()
-    elif sys.argv[param] == "--quiet":
-        print("Updating the ROM boot message to:")
-        print(" Graduate Software - CP/M+ loader.")
-        quietpatch = 1
-        display = 0
-    elif sys.argv[param] == "--password":
-        setpw = 1  # enable password setting
-        newpw = sys.argv[param + 1]  # grab Password
-        newpwlen_crypt = len(newpw) ^ 0xAA
-        newpw_crypt = xorcrypt(
-            newpw, 0xAA, 17
-        )  # adding an extra byte to padding because it throws off the rom size otherwise
-        validate("password", newpw, 16)
-        print("Updating password to:", newpw)
-    elif sys.argv[param] == "--name":
-        setname = 1
-        newname = sys.argv[param + 1]
-        newname_crypt = xorcrypt(newname, 0x4E, 24)
-        validate("name", newname, 24)
-    elif sys.argv[param] == "--address":
-        setaddr = 1
-        newaddress = sys.argv[param + 1]
-        newaddress_crypt = xorcrypt(newaddress, 0x4E, 68)
-        validate("address", newaddress, 68)
-    elif sys.argv[param] == "--serial":
-        setserial = 1
-        newserial = sys.argv[param + 1]
-        newserial_crypt = xorcrypt(newserial, 0x4E, 12)
-        validate("serial", newserial, 12)
-    elif sys.argv[param] == "--src":
-        src = sys.argv[param + 1]
-        sourceset = 1
-    elif sys.argv[param] == "--dest":
-        dest = sys.argv[param + 1]
-        destset = 1
-
-# If no parameters then, we'll assume it was just a filename that was passed, so we switch to display detail mode
-if sourceset == -1:
-    # OK, so if there is no --src, we're going to assume that param 1 is the filename and we're
-    # just doing to do a display details eg same as --src fn.rom --display
-    src = sys.argv[1]
-    sourceset = 1
-    display = 1
-
-# Scan the source rom
-with open(src, "rb") as f:
-    # print("Reading from: "+src)
-    while byte := f.read(1):
-        value = int.from_bytes(byte, "little")
-        if (loc >= 0x1FF0) and (loc <= 0x2009):
-            char = value ^ 0x00
-            if char > 31 and char < 127:
-                copyright = copyright + chr(char)
-        if (loc >= 0x3F00) and (loc <= 0x3F16):
-            char = value ^ 0x4E
-            if char > 31 and char < 127:
-                name = name + chr(char)
-        if (loc > 0x3F19) and (loc < 0x3F5E):
-            char = value ^ 0x4E
-            if char > 31 and char < 127:
-                address = address + chr(char)
-        if (loc > 0x3F70) and (loc < 0x3F7D):
-            char = value ^ 0x4E
-            if char > 31 and char < 127:
-                serial = serial + chr(char)
-        if loc == 0x3F87:
-            char = value ^ 0xAA
-            pwlength = int(char)
-        if (loc >= 0x3F88) and (loc < 0x3FFF):
-            char = value ^ 0xAA
-            if char > 31 and char < 127:
-                password = password + chr(char)
-        loc = loc + 1
-
-    # Validate if its a Graduate software rom, because it needs to be that
+def display_rom_info(rom_bytes):
+    copyright, name, address, serial, password, pwlength = extract_fields(rom_bytes)
     if copyright != "(C)1988 GRADUATE SOFTWARE.":
-        print("Error: This is not a Graduate CPM+ ROM #1")
-        quit()
+        sys.exit("Error: This is not a Graduate CPM+ ROM #1")
+    print(f"\nCopyright: {copyright}")
+    print(f"Serial:   {serial}")
+    print(f"Name:     {name}")
+    print(f"Address:  {address}")
+    print(f"Password: {password} ({pwlength} characters)\n")
 
-if display == 1:
-    print("\nCopyright: " + copyright)
-    print("Serial:   " + serial)
-    print("Name:     " + name)
-    print("Address:  " + address)
-    # print("Password length: ",pwlength)
-    print("Password: " + password + " (" + str(pwlength) + " characters)\n")
+def patch_rom(args, rom_bytes):
+    # Patch the boot message if requested
+    if args.quiet:
+        for k, v in PATCH_OFFSETS.items():
+            rom_bytes[k] = v
 
-# We're going to update some things
-elif setpw == 1 or setname == 1 or setaddr == 1 or sourceset == 1 or quietpatch == 1:
-    # We're going to be updating things, make sure we know where we're writing to
-    if destset == -1:
-        print(
-            "Error: You must specify a destination ROM file using --dest, eg --dest NEWROM.rom"
-        )
-        quit()
-    # And make sure we are'nt overwriting our source
-    if src == dest:
-        print("Error: You cannot have the same file for source and destination.")
-        quit()
+    if args.name:
+        validate("name", args.name, 24)
+        rom_bytes[0x3F00:0x3F17] = xorcrypt(args.name, 0x4E, 24)
+    if args.address:
+        validate("address", args.address, 68)
+        rom_bytes[0x3F1A:0x3F5E] = xorcrypt(args.address, 0x4E, 68)
+    if args.serial:
+        validate("serial", args.serial, 12)
+        rom_bytes[0x3F71:0x3F7D] = xorcrypt(args.serial, 0x4E, 12)
+    if args.password:
+        validate("password", args.password, 16)
+        rom_bytes[0x3F87] = len(args.password) ^ 0xAA
+        pwcrypt = xorcrypt(args.password, 0xAA, 17)
+        # Fill 0x3F88 up to 0x3F97 (max 16 chars, 17 bytes for pad)
+        rom_bytes[0x3F88:0x3F88+len(pwcrypt)] = pwcrypt
 
-    # start copying the ROM
-    loc = 0
-    destfile = open(dest, "wb")
-    with open(src, "rb") as srcfile:
-        # print("Reading from: "+src)
-        while byte := srcfile.read(1):
-            if patchfile[loc] != 0xFF:
-                if loc == 0xBE:
-                    print("Patching boot message: ", end="")
-                elif loc < 0x1C1:
-                    print(".", end="")
-                else:
-                    print(". done!")
+    return rom_bytes
 
-                # print(f'{patchfile[loc]} ',end="")
-                byte = patchfile[loc].to_bytes(1, "little")
-            if (loc >= 0x3F00) and (loc <= 0x3F16) and (setname > -1):
-                # Name, pad(16)
-                if setname == 1:
-                    print("Setting Name...")
-                    destfile.write(newname_crypt)
-                setname = 0  # don't need to rewrite, 0=written
-            elif (loc > 0x3F19) and (loc < 0x3F5D) and (setaddr > -1):
-                if setaddr == 1:
-                    print("Setting Address...")
-                    destfile.write(newaddress_crypt)
-                setaddr = 0
-            elif (loc > 0x3F70) and (loc < 0x3F7D) and (setserial > -1):
-                if setserial == 1:
-                    print("Setting Serial Number...")
-                    destfile.write(newserial_crypt)
-                setserial = 0
-            elif (loc == 0x3F87) and (setpw > -1):
-                num = newpwlen_crypt.to_bytes(1, byteorder="big")
-                destfile.write(bytes(num))
-            elif (loc >= 0x3F88) and (loc < 0x3F98) and (setpw > -1):
-                if setpw == 1:
-                    print("Setting Password...")
-                    for loop in range(0, len(newpw_crypt)):
-                        num = newpw_crypt[loop]
-                        if num == 0xAA:
-                            num = 0x4E
-                        encbyte = num.to_bytes(1, byteorder="big")
-                        destfile.write(encbyte)
-                setpw = 0
-            else:
-                destfile.write(byte)
+def main():
+    args = parse_args()
+    if not args.src:
+        sys.exit("Error: Please specify a source ROM file.")
 
-            loc = loc + 1
+    rom_bytes = bytearray(Path(args.src).read_bytes())
+
+    if args.display:
+        display_rom_info(rom_bytes)
+        return
+
+    if any([args.name, args.address, args.serial, args.password, args.quiet]):
+        if not args.dest:
+            sys.exit("Error: Please specify a destination ROM file using --dest.")
+        if args.src == args.dest:
+            sys.exit("Error: Source and destination must be different files.")
+        patched_rom = patch_rom(args, rom_bytes)
+        Path(args.dest).write_bytes(patched_rom)
+        print(f"Patching complete. Output written to {args.dest}")
+
+if __name__ == "__main__":
+    main()
